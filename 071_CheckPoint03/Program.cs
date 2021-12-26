8b873afe791e1801a5dc5c211ea81eb6033dbd54
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2021-12-26 18:29:19 +0900

    .

---
+++ 071_CheckPoint03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 배열 활용
+/// </summary>
+namespace _071_CheckPoint03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
